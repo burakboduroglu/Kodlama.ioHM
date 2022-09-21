6df4b260121b9a5fa0c4280ba87ae854f3edf738
Burak Boduroğlu <80620802+BurakBoduroglu@users.noreply.github.com>
2022-09-21 23:19:42 +0300

    Add files via upload

---
+++ Java-Camp-2022/Week-1-Homework/Video-11/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i=2;i<10;i+=2){
+            System.out.println(i);
+        }
+        System.out.println("Döngü Bitti");
+    }
+}
---
+++ Java-Camp-2022/Week-1-Homework/Video-20/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        char harf = 'I';
+
+        switch (harf){
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Kalın sesli harf");
+                break;
+            default:
+                System.out.println("İnce sesli harf");
+        }
+    }
+}
