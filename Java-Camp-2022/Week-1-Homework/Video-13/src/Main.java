c26f3db593cb68f3171a3ccb332501583e50f512
Burak Boduroğlu <80620802+BurakBoduroglu@users.noreply.github.com>
2022-09-21 23:12:22 +0300

    Java-Camp-2022

---
+++ Java-Camp-2022/Week-1-Homework/Video-13/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        //for
+        for (int i=2;i<10;i+=2){
+            System.out.println(i);
+        }
+        System.out.println("For Döngüsü Bitti");
+
+        //While
+        int i=2;
+        while (i<10){
+            System.out.println(i);
+            i+=2;
+        }
+        System.out.println("While Döngüsü Bitti");
+
+        //Do-While
+        int j = 100;
+        do {
+            System.out.println(j);
+            j+=2;
+        }while (j<10);
+        System.out.println("Do-While Döngüsü Bitti");
+    }
+}
---
+++ Java-Camp-2022/Week-1-Homework/Video-6/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 12;
+        String mesaj = "Öğrenci sayısı : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+        System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+        System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+    }
+}
