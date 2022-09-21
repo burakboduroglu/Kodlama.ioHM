a9a1dfd15c5ae7f51f1fc8bcd6c29a14fadd8651
Burak Boduroğlu <80620802+BurakBoduroglu@users.noreply.github.com>
2022-09-21 23:18:46 +0300

    Delete Java-Camp-2022/Week-1-Homework directory

--- Java-Camp-2022/Week-1-Homework/Video-7/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        double sayi = 12.5;
-        sayi = -129;
-
-        char karakter = 'A';
-        boolean dogruMu = false;
-    }
-}
