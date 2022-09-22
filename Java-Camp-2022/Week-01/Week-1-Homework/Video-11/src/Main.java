dc8c1c094b5145b8aac9a41f6598576f3cd81b55
Burak Boduroğlu <80620802+BurakBoduroglu@users.noreply.github.com>
2022-09-22 14:08:47 +0300

    Delete Java-Camp-2022/Week-01/Week-1-Homework directory

--- Java-Camp-2022/Week-01/Week-1-Homework/Video-11/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        for (int i=2;i<10;i+=2){
-            System.out.println(i);
-        }
-        System.out.println("Döngü Bitti");
-    }
-}
