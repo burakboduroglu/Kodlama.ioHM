fa4cfbf45d79dcb939f7cc8170b333a0cc89e2d0
Burak Boduroğlu <80620802+BurakBoduroglu@users.noreply.github.com>
2022-09-22 13:51:56 +0300

    Add files via upload

---
+++ Java-Camp-2022/Week-01/Week-1-Homework/Video-12/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i=2;i<10;i+=2){
+            System.out.println(i);
+        }
+        System.out.println("For Döngüsü Bitti");
+
+        int i=2;
+        while (i<10){
+            System.out.println(i);
+            i+=2;
+        }
+        System.out.println("While Döngüsü Bitti");
+    }
+}
