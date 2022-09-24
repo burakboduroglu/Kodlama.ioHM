63e21c77e856f8fa96784d6b0087cab07f55f364
Burak Boduroğlu <80620802+BurakBoduroglu@users.noreply.github.com>
2022-09-24 22:18:42 +0300

    Add files via upload

---
+++ Java-Camp-2022/Week-02/Week-2-Homework/Video-37/src/BaseKrediManager.java
@@ -0,0 +1,5 @@
+public class BaseKrediManager {
+    public void Hesapla(){
+        System.out.println("Kredi hesaplandı");
+    }
+}
---
+++ Java-Camp-2022/Week-02/Week-2-Homework/Video-37/src/KrediUI.java
@@ -0,0 +1,5 @@
+public class KrediUI {
+    public void KrediHesapla(BaseKrediManager baseKrediManager){
+        baseKrediManager.Hesapla();
+    }
+}
